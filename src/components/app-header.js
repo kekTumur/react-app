9d682c55d8a257f421ac76d4381060b020c21e27
Kalim <kalim.ahmethin2003@gamil.com>
2020-07-01 21:24:59 +0300

    second lesson

--- src/components/app-header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const AppHeader = () => <h1>My Todo list</h1>; // React-компонент
-
-export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
+import App from './components/app';
 
 
-const App = () => {
 
-  const toDoData = [
-    {label: 'Drink Cofee', important: false, id: 1},
-    {label: 'Make Awesome App', important: true, id: 2},
-    {label: 'Have a lunch', important: false, id: 3}
-  ];
-
-  return (
-    <div>
-      <AppHeader></AppHeader>
-      <SearchPanel></SearchPanel>
-      <TodoList todos={toDoData}></TodoList>
-    </div>
-  );
-};
-
-
-ReactDOM.render(<App></App>, document.querySelector('#root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
