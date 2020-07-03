d837ce89595a53ff48aadbf81b376a78929eb622
Kalim <kalim.ahmethin2003@gamil.com>
2020-07-03 11:23:15 +0300

    third lesson

---
+++ src/components/add-item/add-item.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './add-item.css';
+
+export default class AddItem extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = event => {
+        this.setState({
+            label: event.target.value.replace(/[^0-9]/g, '') 
+        });
+    };
+
+    onSubmit = event => {
+        event.preventDefault();
+        this.props.onAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
+    render() {
+        const { onAdded } = this.props;
+        return (
+            <form className="add-item-form"
+                  onSubmit={ this.onSubmit }>
+                <input 
+                value={this.state.label}
+                type="text" 
+                className="add-item-input" 
+                placeholder="type to add new note"
+                onChange={this.onLabelChange}
+                ></input>
+                <button 
+                className="add-item-btn btn btn-outline-secondary"
+                >ADD</button>
+            </form>
+        );
+    }
+}
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,37 +4,36 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  onLabelClick = () => {
-    const { label } = this.props;
-
-    
-  };
-
   render() {
+    const {label, onDeleted, onToggleImportant, onToggleDone,
+            important, done} = this.props;
+    let classNames = "todo-list-item";
 
-    const {label, important = false} = this.props;
+    if (done) {
+      classNames += " done";
+    } else classNames = "todo-list-item";
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if (important) {
+      classNames += ' important';
+    }
   
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
-          onClick={ this.onLabelClick }>
+          onClick={ onToggleDone }>
           {label}
         </span>
   
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={ onToggleImportant }>
           <i className="fa fa-exclamation" />
         </button>
   
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
